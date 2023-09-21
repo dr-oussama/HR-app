7944cb1bf5d758c6bb4f28e41eaaced550d959e8
DRAIA Oussama <100196763+Droronoaa@users.noreply.github.com>
2023-09-21 20:48:40 +0100

    Changing background color in update user

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -20,26 +20,48 @@ const UserProfilePage = () => {
     return <div>User not found.</div>;
   }
 
+  const {
+    picture,
+    first_name,
+    last_name,
+    email,
+    phone_number,
+    hire_date,
+    job_title,
+  } = users;
+
   return (
-    <div className="min-h-screen bg-gradient-to-b from-blue-500 to-purple-600 p-4">
+    <div className="min-h-screen bg-blue-500 p-4">
       <div className="flex flex-col items-center mt-8">
-        <Avatar size="large" url={users.picture} />
-        <h2 className="text-3xl font-bold mt-4 text-white">{`${users.first_name} ${users.last_name}`}</h2>
+        <Avatar size="large" url={picture} />
+        <h2 className="text-3xl font-bold mt-4 text-white">{`${first_name} ${last_name}`}</h2>
       </div>
 
       <div className="bg-white rounded-lg shadow-lg mt-8 p-6">
-        <p className="text-lg font-semibold text-gray-800">Email:</p>
-        <p className="text-gray-600">{users.email}</p>
-        <p className="text-lg font-semibold text-gray-800 mt-4">Phone Number:</p>
-        <p className="text-gray-600">{users.phone_number}</p>
-        <p className="text-lg font-semibold text-gray-800 mt-4">Hire Date:</p>
-        <p className="text-gray-600">{users.hire_date}</p>
-        <p className="text-lg font-semibold text-gray-800 mt-4">Job Title:</p>
-        <p className="text-gray-600">{users.job_title}</p>
+        <h3 className="text-2xl font-bold text-gray-800 mb-4">User Information</h3>
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <p className="text-lg font-semibold text-gray-800">Email:</p>
+            <p className="text-gray-600">{email}</p>
+          </div>
+          <div>
+            <p className="text-lg font-semibold text-gray-800">Phone Number:</p>
+            <p className="text-gray-600">{phone_number}</p>
+          </div>
+          <div>
+            <p className="text-lg font-semibold text-gray-800">Hire Date:</p>
+            <p className="text-gray-600">{new Date(hire_date).toLocaleDateString()}</p>
+          </div>
+          <div>
+            <p className="text-lg font-semibold text-gray-800">Job Title:</p>
+            <p className="text-gray-600">{job_title}</p>
+          </div>
+        </div>
       </div>
 
       <div className="bg-white rounded-lg shadow-lg mt-8 p-6">
         <h3 className="text-2xl font-bold text-gray-800 mb-4">Payrolls</h3>
+        {/* Add payroll information here */}
       </div>
     </div>
   );
